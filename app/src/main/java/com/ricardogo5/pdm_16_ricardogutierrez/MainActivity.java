55e239bed9215bd4efae28e8bd2fed5b53d6e488
RicardoGtz <cvricky14608@gmail.com>
2019-03-10 14:50:01 -0600

    Se añadio un boton

--- app/src/main/java/com/ricardogo5/pdm_16_ricardogutierrez/MainActivity.java
+++ app/src/main/java/com/ricardogo5/pdm_16_ricardogutierrez/MainActivity.java
@@ -4,10 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    //Referencia a BluetoothAdapter
+    android.bluetooth.BluetoothAdapter bluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void configurarAdaptadorBluetooth(){
+
+    }
+
+
 }
