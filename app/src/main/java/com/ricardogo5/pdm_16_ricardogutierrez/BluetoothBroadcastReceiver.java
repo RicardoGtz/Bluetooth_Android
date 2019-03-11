bc98a1d2c8ce19cd5b5fa8d9cb643b68aed51c5f
RicardoGtz <cvricky14608@gmail.com>
2019-03-10 19:41:43 -0600

    Boton de encendido y apagado de bluetooth

---
+++ app/src/main/java/com/ricardogo5/pdm_16_ricardogutierrez/BluetoothBroadcastReceiver.java
@@ -0,0 +1,55 @@
+package com.ricardogo5.pdm_16_ricardogutierrez;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Button;
+
+public class BluetoothBroadcastReceiver extends BroadcastReceiver {
+    Button btnBluetooth;
+    public BluetoothBroadcastReceiver(Button btn){
+        super();
+        this.btnBluetooth=btn;
+    }
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        final String action = intent.getAction();
+
+        // Filtramos por la accion. Nos interesa detectar BluetoothAdapter.ACTION_STATE_CHANGED
+        if (BluetoothAdapter.ACTION_STATE_CHANGED.equals(action))
+        {
+            /* Solicitamos la informacion extra del intent etiquetada como BluetoothAdapter.EXTRA_STATE
+               El segundo parametro indicara el valor por defecto que se obtendra si el dato extra no existe */
+            final int estado = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE,BluetoothAdapter.ERROR);
+
+            switch (estado)
+            {
+                // Apagado
+                case BluetoothAdapter.STATE_OFF:
+                {
+                    btnBluetooth.setText(R.string.txtActivarBluetooth);
+                    break;
+                }
+
+                // Encendido
+                case BluetoothAdapter.STATE_ON:
+                {
+                    btnBluetooth.setText(R.string.txtDesactivarBluetooth);
+
+                    // Lanzamos un Intent de solicitud de visibilidad Bluetooth, al que añadimos un par
+                    // clave-valor que indicara la duracion de este estado, en este caso 120 segundos
+                    Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                    discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 120);
+                    btnBluetooth.getContext().startActivity(discoverableIntent);
+
+                    break;
+                }
+                default:
+                    break;
+            }
+
+        }
+    }
+}
--- app/src/main/java/com/ricardogo5/pdm_16_ricardogutierrez/MainActivity.java
+++ app/src/main/java/com/ricardogo5/pdm_16_ricardogutierrez/MainActivity.java
@@ -1,21 +1,107 @@
 package com.ricardogo5.pdm_16_ricardogutierrez;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     //Referencia a BluetoothAdapter
-    android.bluetooth.BluetoothAdapter bluetoothAdapter;
+    private android.bluetooth.BluetoothAdapter bluetoothAdapter;
+    private Button btnBluetoothSet;
+    private BluetoothBroadcastReceiver bbReceiver;
+    private static final int    REQUEST_ENABLE_BT   = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Enlazamos el boton con la vista
+        btnBluetoothSet=findViewById(R.id.btnBluetoothSet);
+        configurarAdaptadorBluetooth();
+
     }
 
     public void configurarAdaptadorBluetooth(){
+        /* Obtenemos el adaptador Bluetooth. Si es NULL, significara que el
+           dispositivo no posee Bluetooth, por lo que deshabilitamos el boton
+           encargado de activar/desactivar esta caracteristica. */
+        bluetoothAdapter= BluetoothAdapter.getDefaultAdapter();
+        if(bluetoothAdapter==null){
+            btnBluetoothSet.setEnabled(false);
+            return;
+        }
+        /* Comprobamos si el Bluetooth esta activo y cambiamos el texto del
+           boton dependiendo del estado. */
+        if(bluetoothAdapter.isEnabled()){
+            btnBluetoothSet.setText(R.string.txtDesactivarBluetooth);
+        }else{
+            btnBluetoothSet.setText(R.string.txtActivarBluetooth);
+        }
+
+        /* Instanciamos un BroadcastReceiver que se encargara de detectar si el estado
+           del Bluetooth del dispositivo ha cambiado mediante su handler onReceive */
+        bbReceiver = new BluetoothBroadcastReceiver(btnBluetoothSet);
+        registrarEventosBluetooth();
 
     }
 
+    private void registrarEventosBluetooth(){
+        // Registramos el BroadcastReceiver que instanciamos previamente para
+        // detectar los distintos eventos que queremos recibir
+        IntentFilter filtro = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+        this.registerReceiver(bbReceiver, filtro);
+    }
 
+    public void changeBluetooth(View view){
+        if(bluetoothAdapter.isEnabled()){
+            bluetoothAdapter.disable();
+        }else{
+            // Lanzamos el Intent que mostrara la interfaz de activacion del
+            // Bluetooth. La respuesta de este Intent se manejara en el metodo
+            // onActivityResult
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
+    /**
+     * Handler del evento desencadenado al retornar de una actividad. En este caso, se utiliza
+     * para comprobar el valor de retorno al lanzar la actividad que activara el Bluetooth.
+     * En caso de que el usuario acepte, resultCode sera RESULT_OK
+     * En caso de que el usuario no acepte, resultCode valdra RESULT_CANCELED
+     */
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        switch(requestCode)
+        {
+            case REQUEST_ENABLE_BT:
+            {
+                if(resultCode == RESULT_OK)
+                {
+                    // Acciones adicionales a realizar si el usuario activa el Bluetooth
+                }
+                else
+                {
+                    // Acciones adicionales a realizar si el usuario no activa el Bluetooth
+                }
+                break;
+            }
+
+            default:
+                break;
+        }
+    }
+
+    // Ademas de realizar la destruccion de la actividad, eliminamos el registro del
+    // BroadcastReceiver.
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        this.unregisterReceiver(bbReceiver);
+    }
 }
